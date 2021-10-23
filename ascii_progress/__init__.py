14cc336789d987144bcf8684832b44335b90de9e
Eric Wolf <robo-eric@gmx.de>
2021-10-23 19:13:55 +0200

    implement issue #4

--- ascii_progress/__init__.py
+++ ascii_progress/__init__.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 
-"""Module for creating ascii spinners and progress bars"""
+"""Module contains ASCII progress indicators"""
+
+# Copyright (c) 2020 Eric W.
+# SPDX-License-Identifier: MIT
 
 import sys
 import math
+from abc import abstractmethod
 from string import Formatter
-from dataclasses import dataclass
-from typing import Sequence, TextIO, Tuple, Iterator, ContextManager
+from typing import Callable, Tuple, ContextManager, Mapping, Any, Union, Iterator, TextIO, Sequence
 
+__all__ = (
+    "Spinner",
+    "LazyFormatter",
+    "LAZY_FORMATTER",
+    "Bar",
+    "ASCIIBar",
+    "ThresholdDecorator",
+    "PercentDecorator",
+    "BarDecorator",
+    "BarFormat"
+)
 __version__ = "0.3"
 __author__ = "Eric Wolf"
 __maintainer__ = "Eric Wolf"
@@ -110,106 +124,347 @@ class Spinner(ContextManager, Iterator[None]):
         self.file.flush()
 
 
-@dataclass
-class BarFormat:
-    """format of a progress bar"""
-    borders: Tuple[str, str]
-    bars: Tuple[str, str]
-    width: int
+class LazyFormatter(Formatter):
+    """formatter which expects functions as arguments"""
 
-    def __post_init__(self) -> None:
-        if len(self.bars[0]) != len(self.bars[1]):
-            raise ValueError("bars not having the same length")
+    __slots__ = ()
 
-    def generate(self, percent: float) -> str:
-        """generate a bar"""
-        bars = int(self.width / 100 * percent)
-        return self.borders[0] + self.bars[1] * bars + self.bars[0] * (self.width - bars) + self.borders[1]
+    def get_value(self, key: Union[int, str], args: Sequence[Any], kwargs: Mapping[str, Any]) -> Any:
+        """retrieve a given field value"""
+        if isinstance(key, str):
+            return kwargs[key]()
+        return args[key]()
 
 
-class BarFormatter(Formatter):
-    """formatter for lazy formatting the format of a bar"""
-    def __init__(self, bar, format: str) -> None:
-        self.bar = bar
-        self.format_string = format
-    
-    def format(self) -> str:
-        return self.vformat(self.format_string, tuple(), {})
-
-    def get_value(self, key, args, kwargs) -> str:
-        if key == "bar":
-            return self.bar.current_bar()
-        elif key == "progress":
-            return self.bar.current_progress()
-        elif key == "percent":
-            return self.bar.current_percent()
-        else:
-            raise KeyError("key not supported")
+LAZY_FORMATTER = LazyFormatter()
 
 
-class Bar(ContextManager, Iterator[None]):
-    """class for creating progress bars"""
-    def __init__(self, bar_format: BarFormat, max: float = 100, format: str = "{bar} {progress} {percent}", file: TextIO = sys.stdout) -> None:
-        """init with format of the bar, progress at 100%, format of the displayed bar and file"""
-        self.bar_format = bar_format
-        self.max = max
-        self.progress = 0.0
-        self.formatter = BarFormatter(self, format)
-        self.file = file
-        self.file.write(self.formatter.format())
-        self.file.flush()
+class Bar(ContextManager["Bar"], Iterator[None]):
+    """abstract base class for progress bars"""
 
-    def __enter__(self):
+    __slots__ = ()
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Bar):
+            return self.progress() == other.progress() \
+                and self.target() == other.target() \
+                and self.width() == other.width()
+        return NotImplemented
+
+    def __enter__(self) -> "Bar":
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, type, value, traceback) -> bool:     # type: ignore
         if type is KeyboardInterrupt:   # handle ^C
             self.replace("\b\bKeyboardInterrupt", end="  \n")
         else:
             self.replace("Finished")
         return False    # we dont handle exceptions
 
-    def __iter__(self):
+    def __iter__(self) -> "Bar":
         return self
 
     def __next__(self) -> None:
-        if self.progress >= self.max:   # reached max iterations
+        if not self.set_progress(self.progress() + 1):
             raise StopIteration
-        else:
-            self.add_progress(1)
-            return None
-    
+        self.update()
+
+    @abstractmethod
+    def update(self) -> None:
+        """redraw bar"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def replace(self, message: str, end: str = "\n") -> None:
+        """replace the bar with a message and end"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def progress(self) -> int:
+        """return the current progress of the bar"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_progress(self, progress: int) -> bool:
+        """set the progress if <= target and return if it was successfull"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def target(self) -> int:
+        """return the progress at 100%"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def width(self) -> int:
+        """return the size of the bar"""
+        raise NotImplementedError
+
+    def ratio(self) -> float:
+        """return the ration progress / target"""
+        return self.progress() / self.target()
+
+
+class ASCIIBar(Bar):
+    """ASCII progress bar"""
+
+    bar_format: "BarFormat"
+
+    current_progress: int
+
+    max: int
+
+    file: TextIO
+
+    __slots__ = ("bar_format", "current_progress", "max", "file")
+
+    def __init__(self, bar_format: "BarFormat", max: int = 100, file: TextIO = sys.stdout) -> None:
+        """init with format of the bar, progress at 100%, format of the displayed bar and file"""
+        self.bar_format = bar_format
+        self.max = max
+        self.current_progress = 1
+        self.file = file
+        self.file.write(
+            LAZY_FORMATTER.format(
+                bar_format.format,
+                bar=self.format_bar,
+                progress=self.format_progress,
+                percent=self.format_percent
+            )
+        )
+        self.file.flush()
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, ASCIIBar):
+            return self.bar_format == other.bar_format \
+                and self.current_progress == other.current_progress \
+                and self.max == other.max \
+                and self.file is other.file
+        return NotImplemented
+
     def update(self) -> None:
         """update bar"""
-        bar = self.formatter.format()
+        bar = LAZY_FORMATTER.format(
+            self.bar_format.format,
+            bar=self.format_bar,
+            progress=self.format_progress,
+            percent=self.format_percent
+        )
         self.file.write("\b" * len(bar))
         self.file.write(bar)
         self.file.flush()
-    
-    def current_progress(self) -> str:
-        """generate progress"""
-        return "{0: >{max_length}.0F}/{1: >{max_length}.0F}".format(self.progress, self.max, max_length=int(math.log10(self.max)) + 1)
-
-    def current_bar(self) -> str:
-        """generate bar"""
-        return self.bar_format.generate(self.progress / (self.max / 100))
-
-    def current_percent(self) -> str:
-        """generate percent"""
-        return "{0: >5.1f}%".format(self.progress / (self.max / 100))
-
-    def add_progress(self, progress: float = 1) -> None:
-        """add progress to bar"""
-        self.progress += progress
-        self.update()
-
-    def set_progress(self, progress: float = 0) -> None:
-        """set progress of bar"""
-        self.progress = progress       
-        self.update()
 
     def replace(self, message: str, end: str = "\n") -> None:
         """replace bar with message"""
-        bar_length = len(self.formatter.format())
+        bar_length = len(
+            LAZY_FORMATTER.format(
+                self.bar_format.format,
+                bar=self.format_bar,
+                progress=self.format_progress,
+                percent=self.format_percent
+            )
+        )
         self.file.write("\b" * bar_length)
-        self.file.write(message + " " * (bar_length - len(message)) + end) # pad message to fully overwrite bar
+        self.file.write(message + " " * (bar_length - len(message)) + end)  # pad message to fully overwrite bar
+
+    def progress(self) -> int:
+        """return the current progress of the bar"""
+        return self.current_progress
+
+    def set_progress(self, progress: int) -> bool:
+        """set the progress if <= target and return if it was successfull"""
+        if 0 <= progress <= self.target():
+            self.current_progress = progress
+            return True
+        return False
+
+    def target(self) -> int:
+        """return the progress at 100%"""
+        return self.max
+
+    def width(self) -> int:
+        """return the size of the bar"""
+        return self.bar_format.width
+
+    def format_bar(self) -> str:
+        """generate a string repersentation of the current bar"""
+        return self.bar_format.generate_bar(self.ratio())
+
+    def format_progress(self) -> str:
+        """generate a string representation of the current progress"""
+        target = self.target()
+        max_length = int(math.log10(target)) + 1
+        return "{0: >{2}.0F}/{1}".format(self.progress(), target, max_length)
+
+    def format_percent(self) -> str:
+        """generate a string representation of the current progress in percent"""
+        return "{0: >4.0%}".format(self.ratio())
+
+
+class ThresholdDecorator(Bar):
+    """decorator which only updates when exceeding thresholds"""
+    bar: Bar
+
+    lower_threshold: int
+
+    upper_threshold: int
+
+    __slots__ = ("bar", "lower_threshold", "upper_threshold")
+
+    def __init__(self, bar: Bar, lower_threshold: int, upper_threshold: int) -> None:
+        self.bar = bar
+        self.lower_threshold = lower_threshold
+        self.upper_threshold = upper_threshold
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, ThresholdDecorator):
+            return self.bar == other.bar \
+                and self.lower_threshold == other.lower_threshold \
+                and self.upper_threshold == other.upper_threshold
+        return NotImplemented
+
+    @abstractmethod
+    def update_thresholds(self) -> None:
+        """update lower and upper thresholds"""
+        raise NotImplementedError
+
+    def update(self) -> None:
+        """redraw bar"""
+        if not self.lower_threshold <= self.bar.progress() < self.upper_threshold:
+            self.bar.update()
+            self.update_thresholds()
+
+    def replace(self, message: str, end: str = "\n") -> None:
+        """replace the bar with a message and end"""
+        self.bar.replace(message, end)
+
+    def progress(self) -> int:
+        """return the current progress of the bar"""
+        return self.bar.progress()
+
+    def set_progress(self, progress: int) -> bool:
+        """set the progress if <= target and return if it was successfull"""
+        return self.bar.set_progress(progress)
+
+    def target(self) -> int:
+        """return the progress at 100%"""
+        return self.bar.target()
+
+    def width(self) -> int:
+        """return the full size of the bar"""
+        return self.bar.width()
+
+
+def calculate_thresholds(bar: Bar, segments: int) -> Tuple[int, int]:
+    """calculate the last and next segment borders for the current progress"""
+    segment = math.floor(bar.ratio() * segments)
+    target = bar.target()
+    return (
+        math.floor(segment / segments * target),
+        math.ceil((segment + 1) / segments * target)
+    )
+
+
+class PercentDecorator(ThresholdDecorator):
+    """decorator which only updates on percent changes"""
+
+    __slots__ = ()
+
+    @classmethod
+    def with_inferred_thresholds(cls, bar: Bar) -> "PercentDecorator":
+        """create an instance with inferred thresholds"""
+        lower_threshold, upper_threshold = calculate_thresholds(bar, 100)
+        return cls(
+            bar,
+            lower_threshold,
+            upper_threshold,
+        )
+
+    def update_thresholds(self) -> None:
+        """update lower and upper thresholds"""
+        self.lower_threshold, self.upper_threshold = calculate_thresholds(self.bar, 100)
+
+
+class BarDecorator(ThresholdDecorator):
+    """decorator which only updates on bar changes"""
+
+    __slots__ = ()
+
+    @classmethod
+    def with_inferred_thresholds(cls, bar: Bar) -> "BarDecorator":
+        """create an instance with inferred thresholds"""
+        lower_threshold, upper_threshold = calculate_thresholds(bar, bar.width())
+        return cls(
+            bar,
+            lower_threshold,
+            upper_threshold,
+        )
+
+    def update_thresholds(self) -> None:
+        """update lower and upper thresholds"""
+        self.lower_threshold, self.upper_threshold = calculate_thresholds(self.bar, self.bar.width())
+
+
+class BarFormat:
+    """format of a progress bar"""
+    borders: Tuple[str, str]
+
+    states: Tuple[str, str]
+
+    width: int
+
+    format: str
+
+    wrapper: Callable[[Bar], Bar]
+
+    __slots__ = ("borders", "states", "width", "format", "wrapper")
+
+    def __init__(self, borders: Tuple[str, str], states: Tuple[str, str], width: int, format: str = "{bar} {progress} {percent}", wrapper: Callable[[Bar], Bar] = lambda b: b) -> None:
+        if len(states[0]) != len(states[1]):
+            raise ValueError("bar states do not have the same length")
+        self.borders = borders
+        self.states = states
+        self.width = width
+        self.format = format
+        self.wrapper = wrapper  # type: ignore
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, BarFormat):
+            return self.borders == other.borders \
+                and self.states == other.states \
+                and self.width == other.width \
+                and self.wrapper is other.wrapper   # type:ignore
+        return NotImplemented
+
+    @classmethod
+    def with_optimized_wrapper(cls, borders: Tuple[str, str], states: Tuple[str, str], width: int, format: str = "{bar} {progress} {percent}") -> "BarFormat":
+        """infer bar update optimizations from the format string"""
+        variables = {v for _, v, _, _ in LAZY_FORMATTER.parse(format) if v is not None}
+        if "progress" in variables:     # changing the bar or percent will always change progress
+            wrapper = lambda b: b
+        elif "percent" in variables:    # changing the bar will always change percent
+            wrapper = lambda b: PercentDecorator(b, 0, math.ceil(b.target() / 100))
+        elif "bar" in variables:        # since nothing always changes the bar it is checked for last
+            wrapper = lambda b: BarDecorator(b, 0, math.ceil(b.target() / b.width()))
+        else:
+            raise ValueError("format string is static or contains invalid variables")
+        return cls(
+            borders,
+            states,
+            width,
+            format,
+            wrapper
+        )
+
+    def generate_bar(self, ratio: float) -> str:
+        """generate a bar representing the ratio progress / target"""
+        size = math.floor(self.width * ratio)
+        return "".join((
+            self.borders[0],
+            self.states[1] * size,
+            self.states[0] * (self.width - size),
+            self.borders[1]
+        ))
+
+    def bar(self, target: int, file: TextIO = sys.stdout) -> Bar:
+        """create a bar object"""
+        return self.wrapper(ASCIIBar(self, target, file))   # type: ignore
--- ascii_progress/__main__.py
+++ ascii_progress/__main__.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 import sys
 import time
-from . import Spinner, Bar, BarFormat
-from .examples import *
+from . import Spinner, BarFormat
 
 # run demo
-for frames in (SPINNER_LINES, SPINNER_ARROWS, SPINNER_BALL, SPINNER_BOUNCING_BALL, SPINNER_BALLONS, SPINNER_DOTS, SPINNER_DOTS_SQUARE, SPINNER_FISH):
+for frames in (
+    "|/-\\",
+    ("←↖↑↗→↘↓↙"),
+    ("◐◓◑◒"),
+    ("(o    )", "( o   )", "(  o  )", "(   o )", "(    o)", "(   o )", "(  o  )", "( o   )"),
+    (".oO@*"),
+    ("", ".", "..", "..."),
+    ("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"),
+    (">))'>", "    >))'>", "        >))'>", "        <'((<", "    <'((<", "<'((<")
+):
     sys.stdout.write("Working ")
-    spinner = Spinner(frames)
-    try:
-        for i in range(1, 10):
+    with Spinner(frames) as spinner:
+        for i in range(1, 15):
             spinner.add_progress()
             time.sleep(0.2)
-    except KeyboardInterrupt:
-        spinner.replace("\b\bKeyboardInterrupt", end="  \n\n")
-    except Exception:
-        spinner.replace("Failed")
-    else:
-        spinner.replace("Done")
-    
-for variant in ((BAR_BORDERS_APT, BAR_BARS_APT), (BAR_BORDERS_PIP, BAR_BARS_PIP), (BAR_BORDERS_CUSTOM, BAR_BARS_CUSTOM), (("-[", "]-"), ("--", "**"))):
+
+for bar_format in map(
+    lambda t: BarFormat(t[0], t[1], 10),
+    (
+        (("[", "]"), (".", "#")),
+        (("|", "|"), (" ", "█")),
+        (("[", "]"), (" ", "="))
+    )
+):
     sys.stdout.write("Working ")
-    bar = Bar(BarFormat(variant[0], variant[1], 10), 100)
-    try:
+    with bar_format.bar(75) as bar:
         for _ in bar:
             time.sleep(0.02)
-    except KeyboardInterrupt:
-        bar.replace("\b\bKeyboardInterrupt", end="  \n\n")
-    except Exception:
-        bar.replace("Failed")
-    else:
-        bar.replace("Done")
-        
